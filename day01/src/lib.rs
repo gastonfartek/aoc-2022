d3516c2c4bd83c2ff0577ff1dac23ca7c6e00ca1
Gaston Fartek <gastonfartek@gmail.com>
2022-12-01 22:15:40 -0500

    day 01 - clean

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,32 +1,24 @@
-pub fn parse_items(input: &str) -> Vec<i32> {
-    let data: Vec<&str> = input.trim().split('\n').map(|l: &str| l.trim()).collect();
-
-    let mut counts: Vec<i32> = Vec::new();
-    let mut count: i32 = 0;
-
-    for item in data {
-        if item.len() == 0 {
-            counts.push(count);
-            count = 0;
-            continue;
-        }
-
-        count += item.parse::<i32>().unwrap();
-    }
-
-    counts.sort();
-    counts.reverse();
-
-    counts
+pub fn parse_items(input: &str) -> Vec<u32> {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|lines| -> u32 {
+            lines
+                .split('\n')
+                .map(|l| l.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect()
 }
 
-pub fn part_a(input: &str) -> i32 {
-    let counts: Vec<i32> = parse_items(input);
-    counts.first().unwrap().clone()
+pub fn part_a(input: &str) -> u32 {
+    let counts: Vec<u32> = parse_items(input);
+    counts.iter().max().unwrap().clone()
 }
 
-pub fn part_b(input: &str) -> i32 {
-    let counts: Vec<i32> = parse_items(input);
+pub fn part_b(input: &str) -> u32 {
+    let mut counts: Vec<u32> = parse_items(input);
+    counts.sort_by(|a, b| b.cmp(a));
     counts.iter().take(3).sum()
 }
 
@@ -34,16 +26,44 @@ pub fn part_b(input: &str) -> i32 {
 mod tests {
     use super::*;
 
+    const SAMPLE_INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn test_sample_parta() {
+        let result: u32 = part_a(SAMPLE_INPUT);
+        println!("max number of calories: {}", result);
+        assert_eq!(result, 24000);
+    }
+    #[test]
+    fn test_sample_partb() {
+        let result: u32 = part_b(SAMPLE_INPUT);
+        println!("top 3 number of calories sum: {}", result);
+        assert_eq!(result, 45000);
+    }
+
     #[test]
     fn test_parta() {
-        let result: i32 = part_a(include_str!("input.txt"));
+        let result: u32 = part_a(include_str!("input.txt"));
         println!("max number of calories: {}", result);
         assert_eq!(result, 65912);
     }
 
     #[test]
     fn test_partb() {
-        let result: i32 = part_b(include_str!("input.txt"));
+        let result: u32 = part_b(include_str!("input.txt"));
         println!("top 3 number of calories sum: {}", result);
         assert_eq!(result, 195625);
     }
