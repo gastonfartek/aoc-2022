8a6f0fcf56dbfb8667943dd1e582658ad3b039f6
Gaston Fartek <gastonfartek@gmail.com>
2022-12-06 15:39:00 -0500

    day06 clean

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,31 +1,16 @@
 use std::collections::HashSet;
 
-pub fn part_a(input: Vec<char>) -> u32 {
+pub fn solve(input: Vec<char>, size: usize) -> usize {
     let mut i = 0;
-
     while i + 4 < input.len() {
-        let set: HashSet<&char> = HashSet::from_iter(input[i..i + 4].iter());
-        if set.len() == 4 {
+        let set: HashSet<&char> = HashSet::from_iter(input[i..i + size].iter());
+        if set.len() == size {
             break;
         }
         i += 1;
     }
 
-    i as u32 + 4
-}
-
-pub fn part_b(input: Vec<char>) -> u32 {
-    let mut i = 0;
-
-    while i + 14 < input.len() {
-        let set: HashSet<&char> = HashSet::from_iter(input[i..i + 14].iter());
-        if set.len() == 14 {
-            break;
-        }
-        i += 1;
-    }
-
-    i as u32 + 14
+    i + size
 }
 
 #[cfg(test)]
@@ -38,25 +23,25 @@ mod tests {
 
     #[test]
     fn test_sample_part_a() {
-        let result = part_a(SAMPLE_INPUT.chars().collect());
+        let result = solve(SAMPLE_INPUT.chars().collect(), 4);
         assert_eq!(result, 7);
     }
 
     #[test]
     fn test_part_a() {
-        let result = part_a(INPUT.chars().collect());
+        let result = solve(INPUT.chars().collect(), 4);
         assert_eq!(result, 1876);
     }
 
     #[test]
     fn test_sample_part_b() {
-        let result = part_b(SAMPLE_INPUT.chars().collect());
+        let result = solve(SAMPLE_INPUT.chars().collect(), 14);
         assert_eq!(result, 19);
     }
 
     #[test]
     fn test_part_b() {
-        let result = part_b(INPUT.chars().collect());
+        let result = solve(INPUT.chars().collect(), 14);
         assert_eq!(result, 2202);
     }
 }
